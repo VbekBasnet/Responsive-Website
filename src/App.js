eae07ce1f4806e76163c3b3ef787d4078869b6ec
vbekbasnet <bibekbasnet1440@gmail.com>
2023-12-20 21:04:18 +0545

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Analystics from "./Components/Analystics";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import Newsletter from "./Components/Newsletter";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analystics />
+      <Newsletter />
     </div>
   );
 }
--- src/Components/Analystics.jsx
+++ src/Components/Analystics.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import laptop from "../Assets/laptop.jpg";
 
 const Analystics = () => {
-  return <div>Analystics</div>;
+  return (
+    <div className="bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-col-2 ">
+        <img className=" w-[500px] mx-auto my-4" src={laptop} alt="" />
+        <div className="uppercase flex flex-col justify-center">
+          <p className="text-[#00df9a] font-bold">Data Analystics dashboard</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Manage data Analystics Centerally
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+            laborum debitis deserunt veritatis, nulla asperiores in excepturi
+            quibusdam! Officia laborum blanditiis soluta recusandae libero
+            tenetur labore vitae rerum? Et, sunt!
+          </p>
+          <button className="bg-[#000000] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Analystics;
